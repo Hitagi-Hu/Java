635f46e74b55717d76354fb3a09cdf4f86e66105
hitagi <1542250379@qq.com>
2018-08-15 00:46:03 +0800

    init

--- src/Algorithm/backtracking/EightQueens/EightQueens.java
+++ src/Algorithm/backtracking/EightQueens/EightQueens.java
@@ -13,6 +13,7 @@ import javafx.stage.Stage;
 public class EightQueens extends Application {
     public static final int SIZE = 8;
     private int[] queens = {-1, -1, -1, -1, -1, -1, -1, -1};
+
     @Override
     public void start(Stage primaryStage) {
         search();
@@ -39,39 +40,40 @@ public class EightQueens extends Application {
         primaryStage.show();
     }
 
-        private boolean search(){
-            int k = 0;      //从第一行开始寻找
-            while(k >= 0 && k < SIZE){
-                int j = findPosition(k); //如果能找到位置，返回该行的位置；否则，返回-1
-                if(j < 0){
-                    queens[k] = -1; //queens[k] == -1表示找不到，需要回溯(k--)
-                    k--;
-                }
-                else{
-                    queens[k] = j;
-                    k++;
-                }
+    private boolean search() {
+        int k = 0;      //从第一行开始寻找
+        while (k >= 0 && k < SIZE) {
+            int j = findPosition(k); //如果能找到位置，返回该行的位置；否则，返回-1
+            if (j < 0) {
+                queens[k] = -1; //queens[k] == -1表示找不到，需要回溯(k--)
+                k--;
+            } else {
+                queens[k] = j;
+                k++;
             }
-            return k != -1;
+        }
+        return k != -1;
 
+    }
+
+    public int findPosition(int k) {
+        int start = queens[k] + 1;
+        for (int j = start; j < SIZE; j++) {
+            if (isValid(k, j))
+                return j;
         }
-        public int findPosition(int k){
-            int start = queens[k] + 1;
-            for(int j = start; j < SIZE; j++)
-            {
-                if(isValid(k, j))
-                    return j;
-            }
-            return -1;
-        }
-        public boolean isValid(int row, int column){
-            for(int i = 1; i <= row; i++)
-                // 同列 || 左上角 || 右上角
-                if(queens[row - i] == column || queens[row - i] == column - i || queens[row - i] == column + i)
-                    return false;
-            return true;
-        }
-        public static void main(String[] args){
+        return -1;
+    }
+
+    public boolean isValid(int row, int column) {
+        for (int i = 1; i <= row; i++)
+            // 同列 || 左上角 || 右上角
+            if (queens[row - i] == column || queens[row - i] == column - i || queens[row - i] == column + i)
+                return false;
+        return true;
+    }
+
+    public static void main(String[] args) {
         Application.launch(args);
-        }
+    }
 }
